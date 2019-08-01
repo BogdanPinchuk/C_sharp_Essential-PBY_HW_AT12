9a6c60cb39eb070aaeedd1b80f3ff7328bd25735
Bogdan <40561032+BogdanPinchuk@users.noreply.github.com>
2019-08-01 23:25:43 +0300

    Додано LesApp2

---
+++ LesApp2/Account.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp2
+{
+    /// <summary>
+    /// Рахунок в банку
+    /// </summary>
+    class Account
+    {
+        /// <summary>
+        /// Обробник стану рахунку
+        /// </summary>
+        /// <param name="str"></param>
+        public delegate void AccountStateHandler(string str);
+
+        /// <summary>
+        /// Сума в банку
+        /// </summary>
+        public int Amount { get; private set; }
+
+        private event AccountStateHandler eWithdraw = null;
+        private event AccountStateHandler eAdded = null;
+
+        /// <summary>
+        /// Зняття грошей з рахунку
+        /// </summary>
+        public event AccountStateHandler EventWithdraw
+        {
+            add { eWithdraw += value; }
+            remove { eWithdraw -= value; }
+        }
+        /// <summary>
+        /// Додавання грошей на рахунок
+        /// </summary>
+        public event AccountStateHandler EventAdded
+        {
+            add { eAdded += value; }
+            remove { eAdded -= value; }
+        }
+
+        /// <summary>
+        /// конструктор користувача
+        /// </summary>
+        /// <param name="amount"></param>
+        public Account(int amount)
+        {
+            this.Amount = amount;
+        }
+
+        /// <summary>
+        /// Кладіння на рахунок
+        /// </summary>
+        /// <param name="money"></param>
+        public void Put(int money)
+        {
+            Amount += money;
+            eAdded($"\n\tНа рахунок поступила наступна сума в розмірі {money:N0} грн.");
+        }
+
+        /// <summary>
+        /// Зняття з рахунку
+        /// </summary>
+        /// <param name="money"></param>
+        public void Withdraw(int money)
+        {
+            Amount -= money;
+            eWithdraw($"\n\tЗ рахунку знята наступна сума в розмірі {money:N0} грн.");
+        }
+
+        /// <summary>
+        /// Вивід на екран повідомлення
+        /// </summary>
+        /// <param name="message"></param>
+        public void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
---
+++ LesApp2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            // join unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // рандом
+            Random rnd = new Random();
+
+            // створення рахунку
+            Account account = new Account(rnd.Next(0, int.MaxValue));
+
+            // info
+            Console.WriteLine($"\n\tДепутатський каунт створено з сумою {account.Amount:N0} грн.");
+
+            // додавання подій
+            account.EventAdded += account.ShowMessage;
+            account.EventWithdraw += account.ShowMessage;
+
+            // ложимо гроші на рахунок
+            account.Put(rnd.Next(1, ushort.MaxValue));
+
+            // знімаємо гроші з рахунку
+            account.Withdraw(rnd.Next(1, ushort.MaxValue));
+
+            // результат рахунку
+            Console.WriteLine($"\n\ttДепутатський акаунт має наступну суму {account.Amount:N0} грн.");
+
+            // repeat
+            DoExitOrRepeat();
+        }
+
+        /// <summary>
+        /// Метод виходу або повторення методу Main()
+        /// </summary>
+        static void DoExitOrRepeat()
+        {
+            Console.WriteLine("\n\nСпробувати ще раз: [т, н]");
+            Console.Write("\t");
+            var button = Console.ReadKey(true);
+
+            if ((button.KeyChar.ToString().ToLower() == "т") ||
+                (button.KeyChar.ToString().ToLower() == "n")) // можливо забули переключити розкладку клавіатури
+            {
+                Console.Clear();
+                Main();
+                // без використання рекурсії
+                //Process.Start(Assembly.GetExecutingAssembly().Location);
+                //Environment.Exit(0);
+            }
+            else
+            {
+                // закриває консоль
+                Environment.Exit(0);
+            }
+        }
+    }
+}
