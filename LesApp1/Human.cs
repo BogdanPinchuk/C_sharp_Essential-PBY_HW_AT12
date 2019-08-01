9861461b2c2fa1bac98da74ce89d6421e87b67f9
Bogdan <40561032+BogdanPinchuk@users.noreply.github.com>
2019-08-01 22:34:23 +0300

    Додано LesApp1

---
+++ LesApp1/Human.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp1
+{
+    delegate void Delegate();
+
+    /// <summary>
+    /// Людина
+    /// </summary>
+    class Human
+    {
+        /// <summary>
+        /// Вид руху
+        /// </summary>
+        public enum TypeOfMove
+        {
+            /// <summary>
+            /// пробіжка 10 разів
+            /// </summary>
+            TenTimes,
+            /// <summary>
+            /// бігати до тих пір доки 5 разі не пригне
+            /// </summary>
+            ToFifeJump
+        }
+
+        /// <summary>
+        /// Рандом, для того щоб були випадкові числа і без повтрень,
+        /// необхідно його створювати зовні
+        /// </summary>
+        private Random rnd = new Random();
+
+        /// <summary>
+        /// Кількість пробіжок
+        /// </summary>
+        public int Iteration { get; private set; } = 10;
+        /// <summary>
+        /// Кількість стрибків
+        /// </summary>
+        public int Count { get; private set; } = 5;
+
+        private event Delegate eJumpE;
+        public event Delegate EventJumpE
+        {
+            add { eJumpE += value; }
+            remove { eJumpE -= value; }
+        }
+
+        /// <summary>
+        /// Двигай =)
+        /// </summary>
+        public void Move(TypeOfMove type)
+        {
+            switch (type)
+            {
+                case TypeOfMove.TenTimes:
+                    for (int i = 0; i < Iteration; i++)
+                    {
+                        if (i == rnd.Next(0, Iteration))
+                        {
+                            eJumpE();
+                        }
+                        else
+                        {
+                            Console.Write("Move ");
+                        }
+                    }
+                    break;
+                case TypeOfMove.ToFifeJump:
+                    for (int i = 0, j = 0; j < Count; i++)
+                    {
+                        if (i == rnd.Next(0, Iteration))
+                        {
+                            eJumpE();
+                            j++;
+                        }
+                        else
+                        {
+                            Console.Write("Move ");
+                        }
+
+                        // захист від зациклення
+                        if (i == Iteration - 1)
+                        {
+                            i = 0;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Пригай
+        /// </summary>
+        public void Jump()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Jump ");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ LesApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesApp1
+{
+    class Program
+    {
+        
+
+        static void Main()
+        {
+            // join unicode
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // створення людини
+            Human human = new Human();
+
+            // Дія яка виконуватиметься при виклику події
+            human.EventJumpE += human.Jump;
+
+            // пускаємо на пробіжку
+            Console.WriteLine($"\n\tБіжимо лише {human.Iteration} разів:\n");
+            human.Move(Human.TypeOfMove.TenTimes);
+
+            // пускаємо на іншу пробіжку
+            Console.WriteLine($"\n\n\tБіжимо до {human.Count} стрибків:\n");
+            human.Move(Human.TypeOfMove.ToFifeJump);
+
+            // repeat
+            DoExitOrRepeat();
+        }
+
+        
+
+        /// <summary>
+        /// Метод виходу або повторення методу Main()
+        /// </summary>
+        static void DoExitOrRepeat()
+        {
+            Console.WriteLine("\n\nСпробувати ще раз: [т, н]");
+            Console.Write("\t");
+            var button = Console.ReadKey(true);
+
+            if ((button.KeyChar.ToString().ToLower() == "т") ||
+                (button.KeyChar.ToString().ToLower() == "n")) // можливо забули переключити розкладку клавіатури
+            {
+                Console.Clear();
+                Main();
+                // без використання рекурсії
+                //Process.Start(Assembly.GetExecutingAssembly().Location);
+                //Environment.Exit(0);
+            }
+            else
+            {
+                // закриває консоль
+                Environment.Exit(0);
+            }
+        }
+
+    }
+}
